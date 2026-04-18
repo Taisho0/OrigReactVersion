aecaa7c56d32ede045de8923e01d88e552ed840f
Homar <homar@example.com>
2026-04-18 08:57:36 +0800

    `Updated package-lock.json and package.json with new dependencies and removed commented out code in FirebaseConfig.js`

--- src/config/FirebaseConfig.js
+++ src/config/FirebaseConfig.js
@@ -15,24 +15,24 @@ import { initializeApp } from "firebase/app";
 //   measurementId: "G-D8ZET4YGN8"
 // };
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBHy4gnb-gf4Hf6O7bLno1ReNo_REduGoU",
-//   authDomain: "originals-website.firebaseapp.com",
-//   projectId: "originals-website",
-//   storageBucket: "originals-website.firebasestorage.app",
-//   messagingSenderId: "368704000921",
-//   appId: "1:368704000921:web:5cfca95d6ad861bd9fb908",
-//   measurementId: "G-J8RYV0HCP9"
-// };
 const firebaseConfig = {
-  apiKey: "AIzaSyDSF-p7jbwvW-SeUTx5q_J2215Ve9iOLG0",
-  authDomain: "test-c5251.firebaseapp.com",
-  projectId: "test-c5251",
-  storageBucket: "test-c5251.firebasestorage.app",
-  messagingSenderId: "865224794512",
-  appId: "1:865224794512:web:6ec23d724d6907f9075315",
-  measurementId: "G-D8ZET4YGN8"
+  apiKey: "AIzaSyBHy4gnb-gf4Hf6O7bLno1ReNo_REduGoU",
+  authDomain: "originals-website.firebaseapp.com",
+  projectId: "originals-website",
+  storageBucket: "originals-website.firebasestorage.app",
+  messagingSenderId: "368704000921",
+  appId: "1:368704000921:web:5cfca95d6ad861bd9fb908",
+  measurementId: "G-J8RYV0HCP9"
 };
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDSF-p7jbwvW-SeUTx5q_J2215Ve9iOLG0",
+//   authDomain: "test-c5251.firebaseapp.com",
+//   projectId: "test-c5251",
+//   storageBucket: "test-c5251.firebasestorage.app",
+//   messagingSenderId: "865224794512",
+//   appId: "1:865224794512:web:6ec23d724d6907f9075315",
+//   measurementId: "G-D8ZET4YGN8"
+// };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
